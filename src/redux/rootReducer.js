03871e26dc32894074b0d4487ec19cc2a7e9f940
Ganesh Prakash Mahajan <ganeshmac@Ganeshs-MacBook-Pro.local>
2018-12-10 06:24:47 +0800

    Combine reducer comment added

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,5 +3,6 @@ import { loginReducer } from './reducers/loginReducer'
 
 const rootReducer = combineReducers({
     loginReducer: loginReducer,
+    // Similarly you can add many reducers here to combine them all together.
 })
 export default rootReducer

852075b313f9d309744a941b32ec5f0efeb679b5
Ganesh Prakash Mahajan <ganeshmac@Ganeshs-MacBook-Pro.local>
2018-12-09 10:35:15 +0800

    HTML generator added for webpack. updated indexjs for generating from webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { render } from 'react-dom'
 
-
-
-
+render(
+	<h1 id='title'
+		className='header'
+		style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
+	Hello World	
+	</h1>,
+	document.getElementById('react-container')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,36 @@
-var path = require('path');
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "assets/bundle.js"
-  },
-  mode: "development",
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: "babel-loader",
-        query: {
-          presets: ["env", "react"]
-        }
-      }
-    ]
-  }
+    entry: "./src/index.js",
+    output: {
+        filename: "assets/bundle.js"
+    },
+    watch:true,
+    mode: "development",
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'My Awesome application',
+            template: './src/index.html',
+            filename: './index.html' //relative to root of the application
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["env", "react"]
+                }
+            }
+        ]
+    }
 }
